5d568dd3a237bad07703538ea1672d4dfa8b176b
b-crawford <william.h.crawford@icloud.com>
2023-03-25 20:05:58 +0000

    Package project

--- src/query_point_algorithms/nth_degree_voronoi.py
+++ src/query_point_algorithms/nth_degree_voronoi.py
@@ -2,7 +2,7 @@ import uuid
 import numpy as np
 from pulp import LpProblem, LpVariable
 from scipy.spatial import Voronoi
-import src.plotting as plot
+import knn_plotting as plot
 import matplotlib.pyplot as plt
 
 
